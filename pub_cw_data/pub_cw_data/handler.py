b6864c7539dccea1400aac06a0ab45bfffa73f52
Richard Barnes-Webb <Richard.Barnes-Webb@paconsulting.com>
2023-02-10 21:23:56 +0000

    main: No changes after this point.

--- pub_cw_data/pub_cw_data/handler.py
+++ pub_cw_data/pub_cw_data/handler.py
@@ -12,32 +12,49 @@ import logging                      #
 log = logging.getLogger(__name__)   # https://docs.aws.amazon.com/lambda/latest/dg/python-logging.html
 log.setLevel(logging.DEBUG)         # https://stackoverflow.com/a/8269542
  
-CLOUDWATCH_NAMESPACE = "rbw/air/pollution01"
-ATTRIBUTES = ["pmten", "pmtwofive"]
+POL_CLOUDWATCH_NAMESPACE = "rbw/air/pollution01"
+POL_ATTRIBUTES = ["pmten", "pmtwofive"]
+
+HUM_CLOUDWATCH_NAMESPACE = "rbw/air/temphumid01"
+HUM_ATTRIBUTES = ["humidity", "temp", "pressure"]
 
 cloudwatch = boto3.client('cloudwatch')
 
 def lambda_handler(event_list, context):
-    log.debug("RBW> Start rbw_air_pollution_pub_cw_data--------------------------------------------------")
+    log.debug("RBW> Start rbw_air_pub_cw_data--------------------------------------------------")
     log.debug("RBW> Received: event_list: %s, context: %s" %(event_list, context))
 
     for event in event_list:
-        log.debug("RBW> event : %s" %(event)) 
-        ts = int(datetime.timestamp(datetime.strptime(event['ts'], '%Y-%m-%d %H:%M:%S'))) if 'ts' in event else 0
+
+        if   all(key in event for key in HUM_ATTRIBUTES) :
+            namespace = HUM_CLOUDWATCH_NAMESPACE
+            attributes = HUM_ATTRIBUTES
+        elif all(key in event for key in POL_ATTRIBUTES) : 
+            namespace = POL_CLOUDWATCH_NAMESPACE
+            attributes = POL_ATTRIBUTES
+        else:
+            log.warn("RBW> Event missing recognised keys : %s" %(event)) 
+            continue
+
         metric_data = []
+        ts = int(datetime.timestamp(datetime.strptime(event['ts'], '%Y-%m-%d %H:%M:%S'))) if 'ts' in event else 0
+
         for key in event:
-            if key in ATTRIBUTES:
-                log.debug("RBW> Catching event key: %s" %(key)) 
-                metric_datum = {
-                    'MetricName': key,
-                    'Value': event[key],
-                    'Timestamp': ts
+            if key == 'ts': continue
+            if key not in attributes: continue
 
-                }
-                metric_data.append(metric_datum)
+            log.debug(f"RBW> datum> {key:15} : {event[key]}")
 
-        print(f"Pushing /{metric_data}/ to cloudWatch {CLOUDWATCH_NAMESPACE}")
-        cloudwatch.put_metric_data(MetricData=metric_data,Namespace=CLOUDWATCH_NAMESPACE) 
+            #NBNBNB: all cloudwatch Values must be ints/floats (!strings)
+            metric_datum = {
+                'MetricName': key,
+                'Value': event[key],
+                'Timestamp': ts
+            }
+            metric_data.append(metric_datum)
 
-    log.debug("RBW>  /End rbw_air_pollution_pub_cw_data--------------------------------------------------")
+        print(f"Pushing /{metric_data}/ to cloudWatch namespace: {namespace}")
+        cloudwatch.put_metric_data(MetricData=metric_data,Namespace=namespace) 
+
+    log.debug("RBW>  /End rbw_air_pub_cw_data--------------------------------------------------")
     return event_list
